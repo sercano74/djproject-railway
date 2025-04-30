e07a1a448af73daa75a473bbd68853a876a8d903
sercano74 <sergio.cannobbio@gmail.com>
2025-04-30 17:58:47 -0400

    psycopg2

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 import dj_database_url
 from dotenv import load_dotenv
-load_dotenv()
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
